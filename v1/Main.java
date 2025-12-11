a3454e63a6ed9defc38c3956c5ff2118789ec3df
zwklz3 <129618167+zwark3@users.noreply.github.com>
2025-12-11 14:55:01 +0100

    Update Main.java

--- v1/Main.java
+++ v1/Main.java
@@ -1,7 +1,3 @@
-import java.beans.MethodDescriptor;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         char[][] plateau = new char[9][9];
